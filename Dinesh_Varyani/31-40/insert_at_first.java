82384b363124f73f8c7789d4082a37179e7b44a1
Tanmay <tanmayyadav300@gmail.com>
2024-11-21 12:07:13 +0530

    upto 45

---
+++ Dinesh_Varyani/31-40/insert_at_first.java
@@ -0,0 +1,9 @@
+public class insert_at_first {
+    private listnode head;
+    private static class listnode{
+        private int data;
+        private listnode next;
+
+        
+    }
+}
--- Dinesh_Varyani/31-40/sll.java
+++
@@ -1,8 +0,0 @@
-public class sll {
- private listnode head;
- private static listnode{
-   private  int data;
-    private listnode  next;
- }
-    
-}
