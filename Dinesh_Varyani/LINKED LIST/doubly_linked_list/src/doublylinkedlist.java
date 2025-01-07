dc060b16b040dd58dfe103e0c0be7a964e346e94
Tanmay <tanmayyadav300@gmail.com>
2025-01-08 01:39:11 +0530

    doublylinked list

--- Dinesh_Varyani/LINKED LIST/doubly_linked_list/src/doublylinkedlist.java
+++ Dinesh_Varyani/LINKED LIST/doubly_linked_list/src/doublylinkedlist.java
@@ -53,7 +53,52 @@ while(temp!=null){
         }
         System.out.println("null");
     }
+    public void insertatbeginning(int value){
+        listnode temp=new listnode(value);
+        if(isempty()){
+            tail =temp;
+        }else{
+            head.previous=temp;
+        }
+        temp.next=head;
+        head=temp;
+        length++;
+    }
+    public void insertatlast(int value){
+        listnode newnode =new listnode(value);
+        if (isempty()){
+            head=newnode;
+        }else {
+            tail.next=newnode;
+            newnode.previous =newnode;
+        }
+        tail=newnode;
+        length++;
+    }
+public void deletefirst(){
+        listnode temp =head.next;
+        if (isempty()){
+            return;
+        }else{
+            temp.previous =null;
+            head.next=null;
 
+        }
+        head =temp;
+        length--;
+}
+    public void deletelast(){
+        listnode temp =tail.previous;
+        if (isempty()){
+            return;
+        }else{
+            temp.next =tail.next;
+            tail.previous=null;
+
+        }
+        tail =temp;
+        length--;
+    }
     public static void main(String[] args) {
         doublylinkedlist dll =new doublylinkedlist();
         dll.insertlast(1);
@@ -62,6 +107,18 @@ while(temp!=null){
         dll.insertlast(4);
         dll.displayforward();
         dll.displaybackward();
+        dll.insertatbeginning(0);
+        dll.insertatlast(5);
+        dll.displayforward();
+        int length1= dll.length();
+        System.out.println(length1);
+        dll.deletefirst();
+        dll.displayforward();
+       int length2= dll.length();
+        System.out.println(length2);
+        dll.deletelast();
+        dll.displayforward();
+        System.out.println(dll.length());
 
     }
 }
