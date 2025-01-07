ffd552a0cc33009055f844a0357f1ddcc014ec03
Tanmay <tanmayyadav300@gmail.com>
2025-01-08 01:40:44 +0530

    some changes and error handled with the help of chatgpt

--- Dinesh_Varyani/LINKED LIST/doubly_linked_list/src/doublylinkedlist.java
+++ Dinesh_Varyani/LINKED LIST/doubly_linked_list/src/doublylinkedlist.java
@@ -3,104 +3,118 @@ public class doublylinkedlist {
     private listnode tail;
     private int length;
 
-    private class listnode{
+    private class listnode {
         private int data;
         private listnode previous;
         private listnode next;
 
-
-        public listnode(int data){
-            this.data=data;
+        public listnode(int data) {
+            this.data = data;
         }
     }
-    public doublylinkedlist(){
-        this.head=null;
-        this.tail=null;
-        this.length=0;
+
+    public doublylinkedlist() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
-    public boolean isempty(){
-        return length==0;
+
+    public boolean isempty() {
+        return length == 0;
     }
-    public int length(){
+
+    public int length() {
         return length;
     }
-    public void insertlast(int value){
-        listnode newnode=new listnode(value);
-        if (isempty()){
-            head=newnode;
-        }
-        else{
-            tail.next=newnode;
-        }
-        newnode.previous=tail;
-        tail=newnode;
-        length++;
 
+    public void insertlast(int value) {
+        listnode newnode = new listnode(value);
+        if (isempty()) {
+            head = newnode;
+        } else {
+            tail.next = newnode;
+            newnode.previous = tail; // Corrected line
+        }
+        tail = newnode;
+        length++;
     }
-    public void displayforward(){
-listnode temp =head;
-while(temp!=null){
-    System.out.print(temp.data+"-->");
-    temp=temp.next;
-}
-        System.out.println("null");
-    }
-    public void displaybackward(){
-        listnode temp =tail;
-        while(temp!=null){
-            System.out.print(temp.data+"-->");
-            temp=temp.previous;
+
+    public void displayforward() {
+        listnode temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " --> ");
+            temp = temp.next;
         }
         System.out.println("null");
     }
-    public void insertatbeginning(int value){
-        listnode temp=new listnode(value);
-        if(isempty()){
-            tail =temp;
-        }else{
-            head.previous=temp;
+
+    public void displaybackward() {
+        listnode temp = tail;
+        while (temp != null) {
+            System.out.print(temp.data + " --> ");
+            temp = temp.previous;
         }
-        temp.next=head;
-        head=temp;
+        System.out.println("null");
+    }
+
+    public void insertatbeginning(int value) {
+        listnode temp = new listnode(value);
+        if (isempty()) {
+            tail = temp;
+        } else {
+            head.previous = temp;
+        }
+        temp.next = head;
+        head = temp;
         length++;
     }
-    public void insertatlast(int value){
-        listnode newnode =new listnode(value);
-        if (isempty()){
-            head=newnode;
-        }else {
-            tail.next=newnode;
-            newnode.previous =newnode;
+
+    public void insertatlast(int value) {
+        listnode newnode = new listnode(value);
+        if (isempty()) {
+            head = newnode;
+        } else {
+            tail.next = newnode;
+            newnode.previous = tail; // Corrected line
         }
-        tail=newnode;
+        tail = newnode;
         length++;
     }
-public void deletefirst(){
-        listnode temp =head.next;
-        if (isempty()){
-            return;
-        }else{
-            temp.previous =null;
-            head.next=null;
 
-        }
-        head =temp;
-        length--;
-}
-    public void deletelast(){
-        listnode temp =tail.previous;
-        if (isempty()){
+    public void deletefirst() {
+        if (isempty()) {
             return;
-        }else{
-            temp.next =tail.next;
-            tail.previous=null;
-
         }
-        tail =temp;
+        if (length == 1) { // Handle single-node case
+            head = null;
+            tail = null;
+        } else {
+            listnode temp = head.next;
+            temp.previous = null;
+            head.next = null;
+            head = temp;
+        }
         length--;
     }
+
+    public void deletelast() {
+        if (isempty()) {
+            return;
+        }
+        if (length == 1) { // Handle single-node case
+            head = null;
+            tail = null;
+        } else {
+            listnode temp = tail.previous;
+            temp.next = null;
+            tail.previous = null;
+            tail = temp;
+        }
+        length--;
+    }
+
     public static void main(String[] args) {
-        doublylinkedlist dll =new doublylinkedlist();
+        doublylinkedlist dll = new doublylinkedlist();
         dll.insertlast(1);
         dll.insertlast(2);
         dll.insertlast(3);
@@ -110,16 +124,12 @@ public void deletefirst(){
         dll.insertatbeginning(0);
         dll.insertatlast(5);
         dll.displayforward();
-        int length1= dll.length();
-        System.out.println(length1);
+        System.out.println("Length: " + dll.length());
         dll.deletefirst();
         dll.displayforward();
-       int length2= dll.length();
-        System.out.println(length2);
+        System.out.println("Length after deleting first: " + dll.length());
         dll.deletelast();
         dll.displayforward();
-        System.out.println(dll.length());
-
+        System.out.println("Length after deleting last: " + dll.length());
     }
 }
-
