f8034bccd7a54fde8b246a6e44c64bfaaa4c622f
Tanmay <tanmayyadav300@gmail.com>
2025-01-21 22:03:03 +0530

    inheritance

---
+++ javabook_by_herbert_schiltd/Inheritance/src/Animal/Dog.java
@@ -0,0 +1,11 @@
+package Animal;
+
+public class Dog extends Animal {
+
+    public static void main(String[] args) {
+    String sound ="woof";
+   Dog dog=new Dog();
+        dog.sayhello(sound);
+    }
+
+}
