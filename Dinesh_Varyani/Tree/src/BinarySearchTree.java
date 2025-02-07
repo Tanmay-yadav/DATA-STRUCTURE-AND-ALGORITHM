72f1efe01d33e031d0ff8461c37e57fd2afcc904
Tanmay <tanmayyadav300@gmail.com>
2025-02-07 23:48:23 +0530

    Insertion in binary search tree and its level order traversal printing the tree

--- Dinesh_Varyani/Tree/src/BinarySearchTree.java
+++ Dinesh_Varyani/Tree/src/BinarySearchTree.java
@@ -1,4 +1,8 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
+
     private treenode root;
     private class treenode{
         private int data;
@@ -9,8 +13,53 @@ public class BinarySearchTree {
             this.data= data;
         }
     }
+    public treenode  insert(treenode root,int value){
+        if (root==null){
+            root =new treenode(value);
+            return root;
+        }
+        if (value<root.data){
+           root.left=insert(root.left,value);
+        }else{
+            root.right=insert(root.right,value);
+        }
+        return root;
+    }
+
+    public void levelOrder (BinarySearchTree.treenode root ){
+        if(root ==null){
+            return;
+        }
+        Queue<BinarySearchTree.treenode> queue =new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            BinarySearchTree.treenode temp= queue.poll();
+            System.out.print(temp.data+ " ");
+            if(temp.left!=null){
+                queue.offer(temp.left);
+
+            }
+            if (temp.right!=null){
+                queue.offer(temp.right);
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
+BinarySearchTree bst= new BinarySearchTree();
+treenode node1= bst.new treenode(5);
+                bst.insert(node1,2);
+                bst.insert(node1,7);
+                bst.insert(node1,3);
+                bst.insert(node1,9);
+                bst.insert(node1,6);
+                bst.insert(node1,1);
+                bst.insert(node1,2);
+
+        System.out.println("the nodes of bst are");
+        bst.levelOrder(node1);
+
 
     }
 }
