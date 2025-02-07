9bef29686e20f96cf18e2ad7667bbe8204cc6d66
Tanmay <tanmayyadav300@gmail.com>
2025-02-07 15:05:05 +0530

    Basic structure of the binary search tree

---
+++ Dinesh_Varyani/Tree/src/BinarySearchTree.java
@@ -0,0 +1,16 @@
+public class BinarySearchTree {
+    private treenode root;
+    private class treenode{
+        private int data;
+        private treenode left;
+        private treenode right;
+
+        public treenode(int data){
+            this.data= data;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
