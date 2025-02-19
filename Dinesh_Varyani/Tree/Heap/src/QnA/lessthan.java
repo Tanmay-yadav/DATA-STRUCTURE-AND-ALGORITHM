f36b96bfdbc2b5786723fb7c8f53be4dff6abab5
Tanmay <tanmayyadav300@gmail.com>
2025-02-19 22:32:04 +0530

    moved the packages here and there.

---
+++ Dinesh_Varyani/Tree/Heap/src/QnA/lessthan.java
@@ -0,0 +1,14 @@
+package QnA;
+
+public class lessthan {
+private treenode root;
+private class treenode{
+    private int data;
+    private treenode left;
+    private treenode right;
+
+    public treenode(int data){
+        this.data=data;
+    }
+}
+}
--- javabook_by_herbert_schiltd/oops/Inheritance/src/Animal/Dog.java
+++ javabook_by_herbert_schiltd/oops/Inheritance/src/Animal/Dog.java
@@ -6,6 +6,8 @@ public class Dog extends Animal {
     String sound ="woof";
    Dog dog=new Dog();
         dog.sayhello(sound);
+        dog.setAge(10);
+        System.out.println(dog.getAge());
     }
 
 }
