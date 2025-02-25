4af4e5acdcb95ffd8f560d64b03afd7bd237f000
Tanmay <tanmayyadav300@gmail.com>
2025-02-25 23:21:15 +0530

    using throw keyword

---
+++ javabook_by_herbert_schiltd/Exception Handling/src/throwdemo.java
@@ -0,0 +1,18 @@
+public class throwdemo {
+    static void demoproc(){
+        try{
+            throw new NullPointerException("demo");
+        } catch (NullPointerException e) {
+            System.out.println("Caught inside demoproc.");
+            throw e; //rethrow the exception
+        }
+    }
+
+    public static void main(String[] args) {
+        try{
+            demoproc();
+        }catch(NullPointerException e){
+            System.out.println("recaught:"+ e);
+        }
+    }
+}
