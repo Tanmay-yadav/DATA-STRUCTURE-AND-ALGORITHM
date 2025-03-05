140c00d36c6e43a1612ff40ed7568a9339c179b8
Tanmay <tanmayyadav300@gmail.com>
2025-03-05 17:16:21 +0530

    Deadlock condition when two threads have a circular dependency on a pair of synchronized objects.

---
+++ javabook_by_herbert_schiltd/Multithreaded Programming/src/Deadlock.java
@@ -0,0 +1,59 @@
+//public class Deadlock {
+    // An example of deadlock.
+    class A{
+        synchronized void foo(B b){
+            String name = Thread.currentThread().getName();
+            System.out.println(name + "entered A.foo");
+            try{
+                Thread.sleep(1000);
+            }catch(Exception e){
+                System.out.println("A Interrupted");
+            }
+            System.out.println(name + " trying to call B.last() ");
+            b.last();
+        }
+        synchronized void last(){
+            System.out.println("Inside A.last");
+        }
+    }
+    class B{
+        synchronized void bar(A a){
+            String name =Thread.currentThread().getName();
+            System.out.println(name + " entered B.bar");
+
+            try{
+                Thread.sleep(1000);
+            }catch (Exception e){
+                System.out.println("B Interrupted");
+            }
+            System.out.println(name +" trying to call A.last()");
+            a.last();
+        }
+        synchronized void last(){
+            System.out.println("Inside A.last");
+        }
+    }
+     class Deadlck implements Runnable{
+        A a =new A();
+        B b = new B();
+        Thread t;
+        Deadlck(){
+            Thread.currentThread().setName("MainThread");
+            t = new Thread(this,"RacingThread");
+        }
+        void deadlockstart(){
+            t.start();
+            a.foo(b); // get lock on b in other thread.
+            System.out.println("Back in the MAin thread");
+        }
+        public void run(){
+            b.bar(a); // get lock on b in other thread.
+            System.out.println("BAck in other thread");
+        }
+
+        public static void main(String[] args) {
+            Deadlck dl = new Deadlck();
+            dl.deadlockstart();
+        }
+    }
+//}
