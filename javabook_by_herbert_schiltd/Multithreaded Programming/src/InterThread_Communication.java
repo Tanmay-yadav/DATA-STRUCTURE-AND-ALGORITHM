d1404ad9e0e961007dc0873efcbaa77ec125d702
Tanmay <tanmayyadav300@gmail.com>
2025-03-05 15:04:50 +0530

    Using notify to notify consumer and producer when to consume and produce.

--- javabook_by_herbert_schiltd/Multithreaded Programming/src/InterThread_Communication.java
+++ javabook_by_herbert_schiltd/Multithreaded Programming/src/InterThread_Communication.java
@@ -2,13 +2,31 @@ public class InterThread_Communication {
     // An incorrect implementation of a producer and consumer.
     static class Q{
         int n;
-        synchronized int get(){
-            System.out.println("Got"+ n);
+        boolean valueSet =false;
+        synchronized int get() {
+            while (!valueSet)
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("InterruptedException Caught ");
+                }
+            System.out.println("Got: " + n);
+            valueSet = false;
+            notify();
             return n;
         }
+
         synchronized void put(int n){
+            while(valueSet)
+                try{
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("InterruptedException Caught");
+                }
             this.n =n;
+            valueSet = true;
             System.out.println("Put: "+ n);
+            notify();
         }
     }
     static class Producer implements Runnable{
