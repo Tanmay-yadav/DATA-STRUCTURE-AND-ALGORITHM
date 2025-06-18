be4362e96dd9c09305824a837404162176ac3ea5
Tanmay <tanmayyadav300@gmail.com>
2025-06-18 11:52:44 +0530

    indentation

--- javabook_by_herbert_schiltd/Lambda Expression/src/lambdaDemo.java
+++ javabook_by_herbert_schiltd/Lambda Expression/src/lambdaDemo.java
@@ -1,6 +1,6 @@
- interface MyNumber{
+interface MyNumber {
     double getValue();
- }
+}
 
 public class lambdaDemo {
     public static void main(String[] args) {
@@ -9,16 +9,16 @@ public class lambdaDemo {
         //Here , the lambda expression is simply a constant expression.
         // when it is assigned to mynum, a class instance is
         //constructed in which the lambda expression implements
-        // the getValue() method in myNumber.
-        mynum= ()-> 123.45;
+        // the getValue() method in myNumber
+        mynum = () -> 123.45;
         // call getvalue(); which is provided by the previously assigned
         //lambda expression.
-        System.out.println("A fixed value: "+ mynum.getValue());
+        System.out.println("A fixed value: " + mynum.getValue());
         //here, a more complex expression is used.
-        mynum =()->Math.random()*100;
+        mynum = () -> Math.random() * 100;
 
         //call the getvalue on the lambda expression above
-        System.out.println("The first random value :"+mynum.getValue());
-        System.out.println("The second random value " +mynum.getValue());
+        System.out.println("The first random value :" + mynum.getValue());
+        System.out.println("The second random value " + mynum.getValue());
     }
 }
