446e7097e878ae3f9ca754b57d837791e924b087
Tanmay <tanmayyadav300@gmail.com>
2025-06-23 13:35:33 +0530

    String concatenation

--- javabook_by_herbert_schiltd/String Handling/src/Basics.java
+++ javabook_by_herbert_schiltd/String Handling/src/Basics.java
@@ -1,2 +1,13 @@
 public class Basics {
+    public static void main(String[] args) {
+
+// Using concatenation to prevent long lines.
+        String longStr ="This could have been "+
+                "a very long line that would "+
+                "wrapped around.But String concatenation "+
+                "prevents this.";
+        System.out.println(longStr);
+        int age =9;
+        System.out.println("The Age of the girl is "+ age+ "years old:");
+    }
 }
